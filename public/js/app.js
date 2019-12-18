8655124ce84d1080436a9f443deb59497b08e92f
zbrush123 <ariel54_y2j@hotmail.com>
2019-12-18 19:30:11 -0400

    make it better

--- public/js/app.js
+++ public/js/app.js
@@ -30,7 +30,7 @@ function agregarMarcador(lat,lng,des){
 
 // Obtener mensajes del servidor
 
-fetch('http://localhost:3000/getSolicitud',{ 
+fetch('https://proysig.herokuapp.com/getSolicitud',{ 
     method: 'GET'
   })
   .then(function(response) {
